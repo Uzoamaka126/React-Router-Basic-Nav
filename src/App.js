fccea0480382855cb4f7d4e94a0ea8fa5e3c024e
Uzoamaka126 <vanessapeters126@gmail.com>
2020-01-16 14:06:30 +0100

    update to the router code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Route } from 'react-router-dom'
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
-  <div>
+  <StyledApp>
     <Navigation />
-  </div>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/contact" component={Contact} />
+
+  </StyledApp>
 );
 
 export default App;
+
